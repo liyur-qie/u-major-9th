ec7ee75d877a2d645ba707823e714b41f2a6f54b
liyur-qie <liyur@outlook.jp>
2024-05-26 19:06:07 +0900

    Create top page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+        port: "",
+        pathname: "/1920x1080.png"
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+        port: "",
+        pathname: "/1280x720.png"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
